5c67391c00fc6be090c21623924e2f2ac72b2e7a
Miguel Encarnação <mjceo@iscte-iul.pt>
2023-05-16 17:03:59 +0100

    [CMU] Updated Logic
    º Removed Dashboard
    º Added Navigation Drawer

--- app/src/main/java/com/cmu/project/main/maps/MapsFragment.kt
+++ app/src/main/java/com/cmu/project/main/maps/MapsFragment.kt
@@ -1,16 +1,24 @@
 package com.cmu.project.main.maps
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
 import android.graphics.Bitmap.createScaledBitmap
 import android.graphics.BitmapFactory.decodeResource
 import android.location.Geocoder
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.cmu.project.R
 import com.cmu.project.core.dialog.CustomDialogFragment
 import com.cmu.project.databinding.FragmentMapsBinding
@@ -20,24 +28,40 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory.fromBitmap
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.firestore.GeoPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MapsFragment : Fragment(), MapsContract.View {
+class MapsFragment : Fragment(), MapsContract.View,
+    NavigationView.OnNavigationItemSelectedListener {
 
     override lateinit var presenter: MapsPresenter
     private lateinit var binding: FragmentMapsBinding
 
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        googleMap.isMyLocationEnabled = false
+        getLocationPermission()
+        googleMap.isMyLocationEnabled = true
+        googleMap.setMapStyle(context?.let {
+            MapStyleOptions.loadRawResourceStyle(
+                it,
+                R.raw.style
+            )
+        })
         setupLibraryMarkers(googleMap)
         setupLibraryDetailsFragment(googleMap)
         setupMapSearchView(googleMap)
     }
+
+    private fun getLocationPermission() {
+        if (checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION) == PERMISSION_GRANTED)
+        else ActivityCompat.requestPermissions(requireActivity(), arrayOf(ACCESS_FINE_LOCATION), 1)
+    }
+
 
     override fun setupMapSearchView(googleMap: GoogleMap) {
         binding.svMapSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
@@ -48,8 +72,21 @@ class MapsFragment : Fragment(), MapsContract.View {
                 }
                 return false
             }
+
             override fun onQueryTextChange(newText: String?): Boolean = false
         })
+
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        binding.btnMenu.setOnClickListener { binding.drawerLayout.openDrawer(Gravity.LEFT) }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navSearch -> findNavController().navigate(R.id.action_mapsFragment_to_bookSearchFragment)
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START);
+        return true
     }
 
     override fun setupLibraryMarkers(googleMap: GoogleMap) {
@@ -57,8 +94,14 @@ class MapsFragment : Fragment(), MapsContract.View {
             presenter.retrieveLibrariesFromCloud().forEach { library ->
                 val position = LatLng(library.location.latitude, library.location.longitude)
                 withContext(Dispatchers.Main) {
-                    val marker = googleMap.addMarker(MarkerOptions().position(position).title(library.name))
-                    val icon = createScaledBitmap(decodeResource(requireContext().resources, R.drawable.iv_search), 80, 80, false)
+                    val marker =
+                        googleMap.addMarker(MarkerOptions().position(position).title(library.name))
+                    val icon = createScaledBitmap(
+                        decodeResource(
+                            requireContext().resources,
+                            R.drawable.iv_search
+                        ), 80, 80, false
+                    )
                     marker?.setIcon(fromBitmap(icon))
                 }
             }
@@ -69,7 +112,11 @@ class MapsFragment : Fragment(), MapsContract.View {
         googleMap.setOnMapClickListener { coordinate ->
             val dialog: CustomDialogFragment? = activity?.let { CustomDialogFragment(it) }
             dialog?.setupAddLibraryDialog()
-            dialog?.binding?.btnAction?.setOnClickListener { dialog.addLibrary(GeoPoint(coordinate.latitude, coordinate.longitude)) }
+            dialog?.binding?.btnAction?.setOnClickListener {
+                dialog.addLibrary(
+                    GeoPoint(coordinate.latitude, coordinate.longitude)
+                )
+            }
         }
 
         googleMap.setOnMarkerClickListener { marker ->
